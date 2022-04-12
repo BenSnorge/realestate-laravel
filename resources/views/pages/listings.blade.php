8ba70a819b97a9553657f9352b233c35c00b4427
Benjamin Sparwasser <benjaminsparwasser@gmail.com>
2022-04-12 14:39:07 +0200

    listings page finished

--- resources/views/pages/listings.blade.php
+++ resources/views/pages/listings.blade.php
@@ -1,30 +1,159 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="listings-page">
-      <div class="listings-city">
-        <img class="listings-city__image" src="https://images.dwell.com/photos-6311371353569939456/6739650785763569664-large/modern-architectural-style-with-warm-finishes-to-compliment-mountain-setting.jpg" alt="">
-        <h1 class="listings-city__title">South Beach</h1>
+<div class="listings-page">
+  <div class="listings-city">
+    <img class="listings-city__image"
+      src="https://images.unsplash.com/photo-1578439297699-eb414262c2de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1640&q=80"
+      alt="">
+    <h1 class="listings-city__title">South Beach</h1>
+  </div>
+  <div class="listings-filter">
+    <div class="listings-filter__wrapper">
+      <div class="listings-filter__option">
+        Any Price <i class="fa-solid fa-caret-down"></i>
       </div>
-      <div class="listings-filter">
-        <div class="listings-filter__wrapper">
-          <div class="listings-filter__option">
-          Any Price <i class="fa-solid fa-caret-down"></i>
-        </div>
-        <div class="listings-filter__option">
-          All Beds <i class="fa-solid fa-caret-down"></i>
-        </div>
-        <div class="listings-filter__option">
-          Hometype <i class="fa-solid fa-caret-down"></i>
-        </div>
-        <div class="listings-filter__option">
-          More <i class="fa-solid fa-caret-down"></i>
-        </div>
-        <div class="listings-filter__button">
-          Search
-        </div>
-        </div>
-        
+      <div class="listings-filter__option">
+        All Beds <i class="fa-solid fa-caret-down"></i>
+      </div>
+      <div class="listings-filter__option">
+        Hometype <i class="fa-solid fa-caret-down"></i>
+      </div>
+      <div class="listings-filter__option">
+        More <i class="fa-solid fa-caret-down"></i>
+      </div>
+      <div class="listings-filter__button">
+        Search
       </div>
     </div>
+
+  </div>
+  <div class="listings-properties">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img
+              src="https://images.unsplash.com/photo-1578439297699-eb414262c2de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1640&q=80"
+              alt="">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i
+                class="fa-solid fa-bath"></i> 3 <i class="fa-solid fa-ruler-combined"></i> 2340 SQFT</span>
+            <span class="listings-properties__item-address">21345 Grand Str, <br>
+              Miami Beach, Fl 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">Bryant Realty
+            </span>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img
+              src="https://images.unsplash.com/photo-1578439297699-eb414262c2de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1640&q=80"
+              alt="">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i
+                class="fa-solid fa-bath"></i> 3
+              <i class="fa-solid fa-ruler-combined"></i> 2340 SQFT</span>
+            <span class="listings-properties__item-address">21345 Grand Str, <br>
+              Miami Beach, Fl 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">Bryant Realty
+            </span>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img
+              src="https://images.unsplash.com/photo-1578439297699-eb414262c2de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1640&q=80"
+              alt="">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i
+                class="fa-solid fa-bath"></i> 3
+              <i class="fa-solid fa-ruler-combined"></i> 2340 SQFT</span>
+            <span class="listings-properties__item-address">21345 Grand Str, <br>
+              Miami Beach, Fl 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">Bryant Realty
+            </span>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img
+              src="https://images.unsplash.com/photo-1578439297699-eb414262c2de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1640&q=80"
+              alt="">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i
+                class="fa-solid fa-bath"></i> 3
+              <i class="fa-solid fa-ruler-combined"></i> 2340 SQFT</span>
+            <span class="listings-properties__item-address">21345 Grand Str, <br>
+              Miami Beach, Fl 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">Bryant Realty
+            </span>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img
+              src="https://images.unsplash.com/photo-1578439297699-eb414262c2de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1640&q=80"
+              alt="">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i
+                class="fa-solid fa-bath"></i> 3
+              <i class="fa-solid fa-ruler-combined"></i> 2340 SQFT</span>
+            <span class="listings-properties__item-address">21345 Grand Str, <br>
+              Miami Beach, Fl 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">Bryant Realty
+            </span>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img
+              src="https://images.unsplash.com/photo-1578439297699-eb414262c2de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1640&q=80"
+              alt="">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i
+                class="fa-solid fa-bath"></i> 3
+              <i class="fa-solid fa-ruler-combined"></i> 2340 SQFT</span>
+            <span class="listings-properties__item-address">21345 Grand Str, <br>
+              Miami Beach, Fl 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">Bryant Realty
+            </span>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="listings-properties__item">
+            <img
+              src="https://images.unsplash.com/photo-1578439297699-eb414262c2de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1640&q=80"
+              alt="">
+            <span class="listings-properties__item-price">$250,000</span>
+            <span class="listings-properties__item-details"><i class="fa-solid fa-bed"></i> 4 <i
+                class="fa-solid fa-bath"></i> 3
+              <i class="fa-solid fa-ruler-combined"></i> 2340 SQFT</span>
+            <span class="listings-properties__item-address">21345 Grand Str, <br>
+              Miami Beach, Fl 23456
+            </span>
+            <div class="listings-properties__item-line"></div>
+            <span class="listings-properties__item-owner">Bryant Realty
+            </span>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</div>
 @endsection
